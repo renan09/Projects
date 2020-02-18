cda3df6785a67478587702dbcc1729a5a74485b0
naren <lvnaren@gmail.com>
2020-02-18 19:02:14 +0530

    Reprocessing

--- Django Project/mysite/myblogapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyblogappConfig(AppConfig):
-    name = 'myblogapp'
--- Django Project/mysite/myblogapp/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class commentForm(forms.Form):
-    comment = forms.CharField(label='comment', max_length=100)
